4444d55e67560d6361729efeae73743292230bc2
MakarovAS <makarovas82@gmail.com>
2016-07-05 07:51:35 +0400

    1

--- timesheet/urls.py
+++ timesheet/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
 
-    url(r'^$', HomePageView.as_view(), name='home'),
+    #url(r'^$', HomePageView.as_view(), name='timesheet'),
     #url(r'^customers/$', CustomersView.as_view(), name='customers'),
     #url(r'^sertificates/$', SertificatesView.as_view(), name='sertificates'),
 ]
