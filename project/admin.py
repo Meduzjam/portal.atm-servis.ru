db9771bd48bcaa49122544d478960898de57f24e
Meduzjam <makarovas82@gmail.com>
2016-07-06 10:25:14 +0400

    1

--- project/admin.py
+++ project/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+class TaskAdmin(admin.ModelAdmin):
+	list_display = ("name","parent.name")
+
 admin.site.register(Project)
-admin.site.register(Task)
+admin.site.register(Task,TaskAdmin)
 admin.site.register(Plan)
 admin.site.register(PlanTask)
