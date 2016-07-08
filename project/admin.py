94d857033ea301fcd72bbc118d424614b83315e9
Meduzjam <makarovas82@gmail.com>
2016-07-08 14:49:17 +0400

    1

--- project/admin.py
+++ project/admin.py
@@ -2,30 +2,26 @@ from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 from .models import *
 
-class TaskAdmin(admin.ModelAdmin):
-	list_display = ("name","parent")
-
 class TaskAdmin(MPTTModelAdmin):
 	# specify pixel amount for this ModelAdmin only:
 	mptt_level_indent = 20
 
-
-class PlanTaskInline(admin.TabularInline):
-	model = PlanTask.task.through
+class ProjectPlanTaskInline(admin.TabularInline):
+	model = ProjectPlan.task.through
 	extra = 10
 	min_num = 1
 	verbose_name = 'задача'
 	verbose_name_plural = 'задачи'
 
-class PlanTaskAdmin(admin.ModelAdmin):
+class ProjectPlanAdmin(admin.ModelAdmin):
 	list_display = ("plan","project")
 
 	inlines = [
-		PlanTaskInline,
+		ProjectPlanTaskInline,
 		]
 
 
 admin.site.register(Project)
 admin.site.register(Task,TaskAdmin)
 admin.site.register(Plan)
-admin.site.register(PlanTask,PlanTaskAdmin)
+admin.site.register(ProjectPlan,ProjectPlanAdmin)
